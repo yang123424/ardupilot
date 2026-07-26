89d75ccf5118215ce7ee2b9bca874222708fddc9
yang123424 <1254879320@qq.com>
2026-07-26 17:38:37 +0800

    创建pos_control_run()函数，还没有修改

--- ArduCopter/mode_modestar.cpp
+++ ArduCopter/mode_modestar.cpp
@@ -22,31 +22,8 @@ bool modestar::init(bool ignore_checks)
 // should be called at 100hz or more
 void modestar::run()
 {
-pos_control_start()
+pos_control_run()
  }
-
-
-// initialise position controller
-void modestar::pva_control_start()
-{
-    // initialise horizontal speed, acceleration
-    pos_control->set_max_speed_accel_xy(wp_nav->get_default_speed_xy(), wp_nav->get_wp_acceleration());
-    pos_control->set_correction_speed_accel_xy(wp_nav->get_default_speed_xy(), wp_nav->get_wp_acceleration());
-
-    // initialize vertical speeds and acceleration
-    pos_control->set_max_speed_accel_z(wp_nav->get_default_speed_down(), wp_nav->get_default_speed_up(), wp_nav->get_accel_z());
-    pos_control->set_correction_speed_accel_z(wp_nav->get_default_speed_down(), wp_nav->get_default_speed_up(), wp_nav->get_accel_z());
-
-    // initialise velocity controller
-    pos_control->init_z_controller();
-    pos_control->init_xy_controller();
-
-    // initialise yaw
-    auto_yaw.set_mode_to_default(false);
-
-    // initialise terrain alt
-    guided_pos_terrain_alt = false;
-}
 
 // initialise guided mode's position controller
 void modestar::pos_control_start()
@@ -56,4 +33,49 @@ void modestar::pos_control_start()
     wp_nav->get_wp_stopping_point(stopping_point);
     wp_nav->set_wp_destination(stopping_point,false);
     auto_yaw.set_mode_to_default(false);
+}
+
+void modestar::pos_control_run()
+{
+    // if not armed set throttle to zero and exit immediately
+    if (is_disarmed_or_landed()) {
+        // do not spool down tradheli when on the ground with motor interlock enabled
+        make_safe_ground_handling(copter.is_tradheli() && motors->get_interlock());
+        return;
+    }
+
+    // calculate terrain adjustments
+    float terr_offset = 0.0f;
+    if (guided_pos_terrain_alt && !wp_nav->get_terrain_offset(terr_offset)) {
+        // failure to set destination can only be because of missing terrain data
+        copter.failsafe_terrain_on_event();
+        return;
+    }
+
+    // set motors to full range
+    motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
+
+    // send position and velocity targets to position controller
+    guided_accel_target_cmss.zero();
+    guided_vel_target_cms.zero();
+
+    // stop rotating if no updates received within timeout_ms
+    if (millis() - update_time_ms > get_timeout_ms()) {
+        if ((auto_yaw.mode() == AutoYaw::Mode::RATE) || (auto_yaw.mode() == AutoYaw::Mode::ANGLE_RATE)) {
+            auto_yaw.set_mode(AutoYaw::Mode::HOLD);
+        }
+    }
+
+    float pos_offset_z_buffer = 0.0; // Vertical buffer size in m
+    if (guided_pos_terrain_alt) {
+        pos_offset_z_buffer = MIN(copter.wp_nav->get_terrain_margin() * 100.0, 0.5 * fabsF(guided_pos_target_cm.z));
+    }
+    pos_control->input_pos_xyz(guided_pos_target_cm, terr_offset, pos_offset_z_buffer);
+
+    // run position controllers
+    pos_control->update_xy_controller();
+    pos_control->update_z_controller();
+
+    // call attitude controller with auto yaw
+    attitude_control->input_thrust_vector_heading(pos_control->get_thrust_vector(), auto_yaw.get_heading());
 }
