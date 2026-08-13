0d7d7c63916a06908dc39d965fa0f0a6c0161a99
YJH <1254879320@qq.com>
2026-08-13 14:56:50 +0800

    尝试修改舵机的trim_pwm

--- libraries/SRV_Channel/SRV_Channel_aux.cpp
+++ libraries/SRV_Channel/SRV_Channel_aux.cpp
@@ -364,9 +364,9 @@ SRV_Channels::set_output_pwm_trimmed(SRV_Channel::Aux_servo_function_t function,
         if (channels[i].function == function) {
             int16_t value2;
             if (channels[i].get_reversed()) {
-                value2 = 1500 - value + channels[i].get_trim();
+                value2 = 1300 - value + channels[i].get_trim();
             } else {
-                value2 = value - 1500 + channels[i].get_trim();
+                value2 = value - 1300 + channels[i].get_trim();
             }
             channels[i].set_output_pwm(constrain_int16(value2,channels[i].get_output_min(),channels[i].get_output_max()));
             channels[i].output_ch();
